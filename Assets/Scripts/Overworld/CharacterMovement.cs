3b1b49640321c43309d2cf7cee21dc8fda1a5f31
Toby Nijman <toby.nijman@jcore.com>
2020-07-03 16:27:48 +0200

    Added movement

---
+++ Assets/Scripts/Overworld/CharacterMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D characterRigidBody;
+    private Vector3 change;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.characterRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.change = Vector3.zero;
+        this.change.x = Input.GetAxisRaw("Horizontal");
+        this.change.y = Input.GetAxisRaw("Vertical");
+
+        this.MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        characterRigidBody.MovePosition(transform.position + this.change * this.speed * Time.deltaTime);
+    }
+}
